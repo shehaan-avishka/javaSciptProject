60c8b016fd252643fd0ab1bf804c710f52e656d3
shehaan-avishka <shehaan.avishka00@gmail.com>
2022-07-08 13:41:58 +0530

    add %10 to the formula

--- script15.js
+++ script15.js
@@ -3,11 +3,11 @@ const o = ["th", "st", "nd", "rd"];
 
 for( let i=0; i< color.length; ++i){
     let j = i+1;
-    if (j==1){
+    if (j%10==1){
         console.log(j+o[1]+ " choice is "+color[i]);
-    }else if(j==2){
+    }else if(j%10==2){
         console.log(j+o[2]+ " choice is "+color[i]);
-    }else if(j==3){
+    }else if(j%10==3){
         console.log(j+o[3]+ " choice is "+color[i]);
     }else{
         console.log(j+o[0]+ " choice is "+color[i]);
